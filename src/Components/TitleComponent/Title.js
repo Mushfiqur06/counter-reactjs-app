57e7b36b7c53fe4ccfa339b27efc9bed7c84073f
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-04-14 23:10:59 +0600

    Create App Title and icon

--- src/Components/TitleComponent/Title.js
+++ src/Components/TitleComponent/Title.js
@@ -1,1 +1,44 @@
-Title.js
+import React, { Component } from 'react'
+import css from './Title.css'
+
+class Title extends Component {
+
+	constructor(props){
+		super(props)
+
+		this.state = {
+			title: 'This is dummy Title',
+			isInputBox: false
+		}
+
+	}
+
+
+	render(){
+
+		let titleOutput = null;
+
+		if(this.state.isInputBox){
+
+		}
+		else{
+			titleOutput = (
+				<div className='d-flex Title'>
+					<h1 className='display-4'>{ this.state.title }</h1>
+					<span className='ml-auto editIcon'>
+						<i className="fas fa-pencil-alt"></i>
+					</span>
+				</div>
+			)
+		}
+
+
+		return (
+			<div>
+				{ titleOutput }
+			</div>
+		)
+	}
+}
+
+export default Title;
