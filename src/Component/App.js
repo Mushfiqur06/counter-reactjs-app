26485e9104f64c770c5d7cbbef127825ff21975d
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-04-14 21:08:20 +0600

    Create something

---
+++ src/Component/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Time Counter React js App</h1>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
