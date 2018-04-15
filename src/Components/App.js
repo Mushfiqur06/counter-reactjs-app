e458fc5b51c84bbf962723741b8eacdefbdfd460
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-04-15 12:56:26 +0600

    Create start method and add into Controller tag

--- src/Components/App.js
+++ src/Components/App.js
@@ -19,6 +19,37 @@ class App extends Component {
 		}
 	}
 
+	getStart(){
+		this.intervalId = setInterval( () => {
+			let min = this.state.time.min;
+			let sec = this.state.time.sec;
+			let mili = this.state.time.mili;
+
+			if(mili >= 10){
+				sec = sec + 1;
+				mili = 0;
+			}
+
+			if(sec >= 60){
+				min = min + 1;
+				sec = 0;
+			}
+
+			this.setState({
+				...this.state,
+				time: {
+					min,
+					sec,
+					mili: mili + 1
+				}
+			})
+		}, 100)
+	}
+
+	getPause(){
+		clearInterval(this.intervalId);
+	}
+
   render() {
     return (
       <div className='App'>
@@ -27,7 +58,10 @@ class App extends Component {
           	<div className='col-sm-8 offset-sm-2'>
           		<Title />
           		<CountDown time={ this.state.time } />
-          		<Controller />
+          		<Controller 
+          			start={ this.getStart.bind(this) }
+          			pause={ this.getPause.bind(this) }
+          		/>
           	</div>
           </div>
         </div>
